8d76825ddba2f7ad7e75ec0b42f4e529780c2cdc
SinorGuppyFace <zachary.rucker35@gmail.com>
2018-11-08 15:05:16 -0600

    Merge conflict from master resolved

--- VisualProgrammingApplication6/Title.vb
+++ VisualProgrammingApplication6/Title.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Title
-<<<<<<< HEAD
+
     Public Sub ShowAboutBox()
         'Display the About dialog box
         Using objAbout As New About
@@ -9,11 +9,8 @@
     Private Sub btnAbout_Click(sender As Object, e As EventArgs) Handles btnAbout.Click
         ShowAboutBox()
     End Sub
-=======
+
     Private Sub btnSettings_Click(sender As Object, e As EventArgs) Handles btnSettings.Click
         Settings.Show()
     End Sub
-
-
->>>>>>> upstream/master
 End Class
