1bdf6a18cd4622fb106026d29e7a3aabb93e85d9
David Goforth <goforthd@dlstx.org>
2018-11-15 19:17:04 -0600

    Coding fixes/changes for readability/basic errors
    Updates the code for readability and bug-checking in the various windows. No updates performed on the custom settings window and the actual game window.

--- VisualProgrammingApplication6/About.vb
+++ VisualProgrammingApplication6/About.vb
@@ -1,13 +1,12 @@
 ﻿Public Class About
+
     Private Sub About_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Dim ApplicationTitle As String
-        ApplicationTitle = "Who Wants to be a Millionaire"
+
+        Dim ApplicationTitle As String = "Who Wants to be a Millionaire"
+
         Me.lblAbout.Text = ApplicationTitle
         Me.lblVersion.Text = String.Format("Version {0}", My.Application.Info.Version.ToString)
-        Me.txtGameDescription.Text = "*Who Wants to Be a Millionaire* is a trivia game testing yourskills - determining if you are good enough to win the awesome prize at the end... 
-        Are you smart enough?"
-
-
+        Me.txtGameDescription.Text = "Who Wants to Be a Millionaire! is a trivia game which test your skills about various topics in order to determine whether or not you are smart enough to win the awesome prize at the end. Are you smart enough?"
 
     End Sub
 
--- VisualProgrammingApplication6/Title.vb
+++ VisualProgrammingApplication6/Title.vb
@@ -1,86 +1,127 @@
 ﻿Public Class Title
-	Public Questions As List(Of Question)
 
-	Public Sub ShowAboutBox()
-		'Display the About dialog box
-		Using objAbout As New About
-			objAbout.ShowDialog(Me)
-		End Using
-	End Sub
-	Private Sub btnAbout_Click(sender As Object, e As EventArgs) Handles btnAbout.Click
-		ShowAboutBox()
-	End Sub
+    Public Questions As List(Of Question)
 
-	Private Sub btnSettings_Click(sender As Object, e As EventArgs) Handles btnSettings.Click
-		Settings.Show()
-	End Sub
+    Public Sub ShowAboutBox()
 
-	Private Sub btnCustom_Click(sender As Object, e As EventArgs) Handles btnCustom.Click
-		Custom.Show()
-	End Sub
+        'Display the About dialog box
+        Using objAbout As New About
+
+            objAbout.ShowDialog(Me)
+
+        End Using
+
+    End Sub
+
+    Private Sub btnAbout_Click(sender As Object, e As EventArgs) Handles btnAbout.Click
+
+        ShowAboutBox()
+
+    End Sub
+
+    Private Sub btnSettings_Click(sender As Object, e As EventArgs) Handles btnSettings.Click
+
+        Settings.Show()
+
+    End Sub
+
+    Private Sub btnCustom_Click(sender As Object, e As EventArgs) Handles btnCustom.Click
+
+        Custom.Show()
+
+    End Sub
 
     Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+
         Game.Show()
+
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
         changeColorTitle()
+
     End Sub
 
     Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
         changeColorMoney()
+
     End Sub
 
     Sub changeColorTitle()
+
         If lblTitle.ForeColor = Color.Gold Then
+
             lblTitle.ForeColor = Color.ForestGreen
-        Else lblTitle.ForeColor = Color.Gold
+
+        Else
+
+            lblTitle.ForeColor = Color.Gold
 
         End If
+
     End Sub
 
     Sub changeColorMoney()
+
         If lblDollar1.ForeColor = Color.Gold Then
+
             lblDollar1.ForeColor = Color.ForestGreen
-        Else lblDollar1.ForeColor = Color.Gold
+
+        Else
+
+            lblDollar1.ForeColor = Color.Gold
 
         End If
+
         If lblDollar2.ForeColor = Color.Gold Then
+
             lblDollar2.ForeColor = Color.ForestGreen
-        Else lblDollar2.ForeColor = Color.Gold
+
+        Else
+
+            lblDollar2.ForeColor = Color.Gold
 
         End If
+
     End Sub
 
     Private Sub Title_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-		' Initialize default questions
-		Questions = New List(Of Question)
 
-		Questions.Add(New Question("Do you know the muffin man?", 'Prompt
-									"The one on Drury Lane?", 'Correct
-									"What?",
-									"I thought this was a trivia game",
-									"I think maybe no?"))
-		Questions.Add(New Question("What planet is closest to the sun?", 'Prompt
-									"Mercury", 'Correct
-									"Earth",
-									"Venus",
-									"Gallifrey"))
-		Questions.Add(New Question("Who started the Protestant Reformation?", 'Prompt
-									"Martin Luther", 'Correct
-									"Ulrich Zwingli",
-									"Werner von Protestant",
-									"The Pope"))
-		Questions.Add(New Question("Where did potatoes originate?", 'Prompt
-									"The Americas", 'Correct
-									"Russia",
-									"Ireland",
-									"in the dirt lol"))
-		Questions.Add(New Question("Who was the first president of the USA?", 'Prompt
-									"George Washington", 'Correct
-									"Ganondorf",
-									"Donald Trump",
-									"John Adams"))
-	End Sub
+        ' Initialize default questions
+        Questions = New List(Of Question)
+
+        Questions.Add(New Question("Do you know the muffin man?", 'Prompt
+                                    "The one on Drury Lane?", 'Correct
+                                    "What?",
+                                    "I thought this was a trivia game",
+                                    "I think maybe no?"))
+
+        Questions.Add(New Question("What planet is closest to the sun?", 'Prompt
+                                    "Mercury", 'Correct
+                                    "Earth",
+                                    "Venus",
+                                    "Gallifrey"))
+
+        Questions.Add(New Question("Who started the Protestant Reformation?", 'Prompt
+                                    "Martin Luther", 'Correct
+                                    "Ulrich Zwingli",
+                                    "Werner von Protestant",
+                                    "The Pope"))
+
+        Questions.Add(New Question("Where did potatoes originate?", 'Prompt
+                                    "The Americas", 'Correct
+                                    "Russia",
+                                    "Ireland",
+                                    "in the dirt lol"))
+
+        Questions.Add(New Question("Who was the first president of the USA?", 'Prompt
+                                    "George Washington", 'Correct
+                                    "Ganondorf",
+                                    "Donald Trump",
+                                    "John Adams"))
+
+    End Sub
 
 End Class
